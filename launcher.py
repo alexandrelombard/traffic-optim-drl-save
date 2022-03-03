45925bc6b78e0e319a99cd92393b0f221e28088e
Alexandre Lombard <alexandre.lombard@utbm.fr>
2022-03-03 15:52:08 +0100

    Not important but committing the change in launcher.py (training mode, display false for traffic lights)

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 from environment import *
 
-environment = Environment(mode=TRAFFIC_LIGHTS, display=True, training=True, simulation_time=1000, image_size=50,
+environment = Environment(mode=TRAFFIC_LIGHTS, display=False, training=True, simulation_time=1000, image_size=50,
                           reward_type="mix_vehicle_time", coef=10, flow1=700, flow2=700, flow3=700, flow4=700)
 environment.launch_simulation()
